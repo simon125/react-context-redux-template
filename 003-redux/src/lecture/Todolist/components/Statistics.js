7f6f03f4e9e30920cae42f30fd44895c931e871e
Szymon Oleszek <szymqwerty@gmail.com>
2024-06-30 16:04:01 +0200

    finish2

--- 003-redux/src/lecture/Todolist/components/Statistics.js
+++ 003-redux/src/lecture/Todolist/components/Statistics.js
@@ -1,17 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { PieChart, Pie, Tooltip } from "recharts";
 
 export const Statistics = () => {
-  const total = 10;
-  const amountOfFinishedTodos = 4;
+  const todos = useSelector((store) => store.todos.list);
+
+  const dataToDisplay = todos.reduce(
+    (acc, todoItem) => {
+      if (todoItem.status === true) {
+        acc[0].value += 1;
+      } else {
+        acc[1].value += 1;
+      }
+      return acc;
+    },
+    [
+      { name: "Done", value: 0 },
+      { name: "Todo", value: 0 },
+    ]
+  );
 
   return (
     <article>
-      <p>
-        Total: <b>{total}</b>
-      </p>
-      <p>
-        Done: <b>{amountOfFinishedTodos}</b>
-      </p>
+      <PieChart width={400} height={400}>
+        <Pie
+          dataKey="value"
+          data={dataToDisplay}
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          fill="#8884d8"
+          label
+        />
+        <Tooltip />
+      </PieChart>
     </article>
   );
 };
